d03f45bac4cc50700851804beacff30959a4956a
Erick Aitchison <eaitchis@uci.edu>
2021-10-11 11:00:08 -0700

    PP kernels

--- src/kernels/cuda/coulomb/cuda_coulomb_pp.cu
+++ src/kernels/cuda/coulomb/cuda_coulomb_pp.cu
@@ -6,38 +6,43 @@
 
 
 __global__ 
-static void CUDA_Coulomb_PP_Lagrange(int fid_high_ind, int cluster_num_sources, int cluster_idx_start,
+static void CUDA_Coulomb_PP_Lagrange(int cluster_num_sources, int cluster_idx_start,
                                      int target_x_low_ind, int target_y_low_ind, int target_z_low_ind,
+                                     int target_x_high_ind, int target_y_high_ind, int target_z_high_ind,
                                      int target_x_dim_glob, int target_y_dim_glob, int target_z_dim_glob,
                                      double target_xmin, double target_ymin, double target_zmin,
                                      double target_xdd, double target_ydd, double target_zdd,
                                      double *source_x, double *source_y, double *source_z, double *source_q,
-                                     double *temporary_potential )
+                                     double *potential )
 {
-    int fid=threadIdx.x + blockDim.x * blockIdx.x;
+    int ix=threadIdx.x + blockDim.x * blockIdx.x;
+    int iy=threadIdx.y + blockDim.y * blockIdx.y;
+    int iz=threadIdx.z + blockDim.z * blockIdx.z;
     int target_yz_dim = target_y_dim_glob * target_z_dim_glob;
 
-    if(fid < fid_high_ind*cluster_num_sources){
-        int tid = fid/cluster_num_sources;
+    if(ix >= target_x_low_ind && ix <= target_x_high_ind &&
+       iy >= target_y_low_ind && iy <= target_y_high_ind &&
+       iz >= target_z_low_ind && iz <= target_z_high_ind){
 
-        int j = fid-tid*cluster_num_sources;
-        int ix = tid/target_yz_dim; int tmp = tid - ix*target_yz_dim;
-        int iy = tmp/target_z_dim_glob;
-        int iz = tmp%target_z_dim_glob;
+        double tx = target_xmin + (ix - target_x_low_ind) * target_xdd;
+        double ty = target_ymin + (iy - target_y_low_ind) * target_ydd;
+        double tz = target_zmin + (iz - target_z_low_ind) * target_zdd;
+        int ii = (ix * target_yz_dim) + (iy * target_z_dim_glob) + iz;
+        double temporary_potential = 0.0;
+        for (int j=0;j < cluster_num_sources;j++){
 
-        double tx = target_xmin + ix * target_xdd;
-        double ty = target_ymin + iy * target_ydd;
-        double tz = target_zmin + iz * target_zdd;
-
-        int jj = cluster_idx_start + j;
-        double dx = tx - source_x[jj];
-        double dy = ty - source_y[jj];
-        double dz = tz - source_z[jj];
-        double r  = sqrt(dx*dx + dy*dy + dz*dz);
-        if (r > DBL_MIN) {
-            temporary_potential[j+cluster_num_sources*fid_high_ind] = source_q[jj] / r;
+            int jj = cluster_idx_start + j;
+            double dx = tx - source_x[jj];
+            double dy = ty - source_y[jj];
+            double dz = tz - source_z[jj];
+            double r  = sqrt(dx*dx + dy*dy + dz*dz);
+            if (r > DBL_MIN) {
+                      temporary_potential += source_q[jj] / r;
+             }
         }
+            potential[ii]+= temporary_potential;
     }
+return;
 }
 
 
@@ -53,63 +58,23 @@ void K_CUDA_Coulomb_PP(
     double *source_x, double *source_y, double *source_z, double *source_q,
     struct RunParams *run_params, double *potential, int gpu_async_stream_id)
 {
-    printf("Before Kernel call :::: \n");
-    printf("1. Allocating Host and Device working memory ...\n");
-    cudaError_t cudaErr;
-    double *h_temp_pot,*d_temporary_potential,*potential2;
 
-    int fid_high_ind = (target_x_high_ind - target_x_low_ind)*
-                       (target_y_high_ind - target_y_low_ind)*
-                       (target_z_high_ind - target_z_low_ind);
-    cudaErr = cudaMallocHost(&h_temp_pot, sizeof(double)*cluster_num_sources*fid_high_ind);
-    if ( cudaErr != cudaSuccess )
-        printf("Host malloc failed with error \"%s\".\n", cudaGetErrorString(cudaErr));
-    cudaErr = cudaMalloc(&d_temporary_potential, sizeof(double)*cluster_num_sources*fid_high_ind);
-    if ( cudaErr != cudaSuccess )
-        printf("Device malloc failed with error \"%s\".\n", cudaGetErrorString(cudaErr));
+    int threadsperblock = 8;
+    dim3 nthreads(threadsperblock,threadsperblock,threadsperblock);
+    dim3 nblocks((target_x_high_ind-target_x_low_ind)/threadsperblock + 1,
+                 (target_y_high_ind-target_y_low_ind)/threadsperblock + 1,
+                 (target_z_high_ind-target_z_low_ind)/threadsperblock + 1);
 
-    printf("2. Sending Device memories to Kernel ...\n");
-
-    int nthreads = 512;
-    int nblocks = ((cluster_num_sources * fid_high_ind))/ nthreads + 1;
-
-    CUDA_Coulomb_PP_Lagrange<<<nblocks,nthreads>>>(fid_high_ind, cluster_num_sources, cluster_idx_start,
+    CUDA_Coulomb_PP_Lagrange<<<nblocks,nthreads>>>(cluster_num_sources, cluster_idx_start,
                                      target_x_low_ind, target_y_low_ind, target_z_low_ind,
+                                     target_x_high_ind, target_y_high_ind, target_z_high_ind,
                                      target_x_dim_glob, target_y_dim_glob, target_z_dim_glob,
                                      target_xmin, target_ymin, target_zmin,
                                      target_xdd, target_ydd, target_zdd,
                                      source_x, source_y, source_z,source_q,
-                                     d_temporary_potential );
-        cudaErr = cudaDeviceSynchronize();
-    if ( cudaErr != cudaSuccess )
-        printf("Kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaErr));
+                                     potential );
+     cudaDeviceSynchronize();
 
-    printf("3. Downloading Device memory to Host ...\n");
-    cudaErr = cudaMemcpy(h_temp_pot,d_temporary_potential, (cluster_num_sources) * fid_high_ind *sizeof(double),cudaMemcpyDeviceToHost);
-    if ( cudaErr != cudaSuccess )
-        printf("Device to Host MemCpy failed with error \"%s\".\n", cudaGetErrorString(cudaErr));
-
-    printf("4. Collecting cluster potentials on Host ...\n");
-//       potential2 = (double*) malloc(11*cluster_num_sources * fid_high_ind *sizeof(double));
-        int target_yz_dim = target_y_dim_glob * target_z_dim_glob;
-        for (int tid = 0; tid <= fid_high_ind; tid++) {
-        int ix = tid/target_yz_dim; int tmp = tid - ix*target_yz_dim;
-        int iy = tmp/target_z_dim_glob;
-        int iz = tmp%target_z_dim_glob;
-        ix = ix + target_x_low_ind;
-        iy = iy + target_y_low_ind;
-        iz = iz + target_z_low_ind;
-        int ii = (ix * target_yz_dim) + (iy * target_z_dim_glob) + iz;
-        for (int j = 0; j < cluster_num_sources; j++) {
-            potential[ii] += h_temp_pot[j+cluster_num_sources*fid_high_ind];
-            printf("new pp %i %15.6e\n",ii, potential[ii]);
-        }
-       }
-    printf("5. Cleaning up working memory ...\n");
-    cudaFree(h_temp_pot);
-    cudaFree(d_temporary_potential);
-
-    printf("Exiting :::: \n");
 
     return;
 }
--- src/kernels/cuda/tcf/cuda_tcf_pp.cu
+++ src/kernels/cuda/tcf/cuda_tcf_pp.cu
@@ -6,10 +6,50 @@
 
 
 __global__ 
-static void compute()
+static void CUDA_TCF_PP_Lagrange(double eta, double kap, double kap_eta_2, int cluster_num_sources, int cluster_idx_start,
+                                     int target_x_low_ind, int target_y_low_ind, int target_z_low_ind,
+                                     int target_x_high_ind, int target_y_high_ind, int target_z_high_ind,
+                                     int target_x_dim_glob, int target_y_dim_glob, int target_z_dim_glob,
+                                     double target_xmin, double target_ymin, double target_zmin,
+                                     double target_xdd, double target_ydd, double target_zdd,
+                                     double *source_x, double *source_y, double *source_z, double *source_q,
+                                     double *potential )
 {
-    printf("Hello TCF_PP thread %d, block %d\n", threadIdx.x, blockIdx.x);
+    int ix=threadIdx.x + blockDim.x * blockIdx.x;
+    int iy=threadIdx.y + blockDim.y * blockIdx.y;
+    int iz=threadIdx.z + blockDim.z * blockIdx.z;
+    int target_yz_dim = target_y_dim_glob * target_z_dim_glob;
+
+    if(ix >= target_x_low_ind && ix <= target_x_high_ind &&
+       iy >= target_y_low_ind && iy <= target_y_high_ind &&
+       iz >= target_z_low_ind && iz <= target_z_high_ind){
+
+        double tx = target_xmin + (ix - target_x_low_ind) * target_xdd;
+        double ty = target_ymin + (iy - target_y_low_ind) * target_ydd;
+        double tz = target_zmin + (iz - target_z_low_ind) * target_zdd;
+        int ii = (ix * target_yz_dim) + (iy * target_z_dim_glob) + iz;
+        double temporary_potential = 0.0;
+        for (int j=0;j < cluster_num_sources;j++){
+
+            int jj = cluster_idx_start + j;
+            double dx = tx - source_x[jj];
+            double dy = ty - source_y[jj];
+            double dz = tz - source_z[jj];
+            double r  = sqrt(dx*dx + dy*dy + dz*dz);
+            if (r > DBL_MIN) {
+                      double kap_r = kap *r;
+                      double r_eta = r / eta;
+                      temporary_potential += source_q[jj] / r * (exp(-kap_r) * erfc(kap_eta_2 - r_eta)
+                                             - exp(kap_r) * erfc(kap_eta_2 + r_eta));
+             }
+        }
+            potential[ii]+= temporary_potential;
+    }
+return;
+
 }
+
+
 
 
 __host__
@@ -27,9 +67,46 @@ void K_CUDA_TCF_PP(
     double kap = run_params->kernel_params[0];
     double eta = run_params->kernel_params[1];
     double kap_eta_2 = kap * eta / 2.0;
+    int threadsperblock = 8;
+    dim3 nthreads(threadsperblock,threadsperblock,threadsperblock);
+    dim3 nblocks((target_x_high_ind-target_x_low_ind)/threadsperblock + 1,
+                 (target_y_high_ind-target_y_low_ind)/threadsperblock + 1,
+                 (target_z_high_ind-target_z_low_ind)/threadsperblock + 1);
 
-    compute<<<1,32>>>();
-    cudaDeviceSynchronize();
+    CUDA_TCF_PP_Lagrange<<<nblocks,nthreads>>>(eta,kap,kap_eta_2,cluster_num_sources, cluster_idx_start,
+                                      target_x_low_ind,target_y_low_ind,target_z_low_ind,
+                                     target_x_high_ind,target_y_high_ind,target_z_high_ind,
+                                     target_x_dim_glob,target_y_dim_glob,target_z_dim_glob,
+                                     target_xmin,target_ymin,target_zmin,
+                                     target_xdd,target_ydd,target_zdd,
+                                     source_x, source_y, source_z, source_q,
+                                     potential );
+    //cudaDeviceSynchronize();
+    cudaError_t cudaErr;
+       cudaErr = cudaDeviceSynchronize();
+    if ( cudaErr != cudaSuccess )
+        printf("Kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaErr));
+
+    int target_yz_dim = target_y_dim_glob * target_z_dim_glob;
+    int target_xyz_dim = target_x_dim_glob*target_yz_dim;
+    double *h_pot;
+    cudaErr = cudaMallocHost(&h_pot, target_xyz_dim*sizeof(double));
+    if ( cudaErr != cudaSuccess )
+        printf("Host malloc failed with error \"%s\".\n", cudaGetErrorString(cudaErr));
+    cudaErr = cudaMemcpy(h_pot, potential, target_xyz_dim*sizeof(double), cudaMemcpyDeviceToHost);
+    if ( cudaErr != cudaSuccess )
+        printf("Device to Host MemCpy failed with error \"%s\".\n", cudaGetErrorString(cudaErr));
+    for (int ix = target_x_low_ind; ix <= target_x_high_ind; ix++) {
+    for (int iy = target_y_low_ind; iy <= target_y_high_ind; iy++) {
+    for (int iz = target_z_low_ind; iz <= target_z_high_ind; iz++) {
+        int ii = (ix * target_yz_dim) + (iy * target_z_dim_glob) + iz;
+        printf("direct potential, %d %15.6e\n", ii, h_pot[ii]);
+    }
+    }
+    }
+
+    cudaFree(h_pot);
 
     return;
+
 }
